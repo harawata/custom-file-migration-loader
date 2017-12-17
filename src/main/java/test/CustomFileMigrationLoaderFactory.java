501f13ba4eefdb446bdd8ee9773adfd445c541c0
Iwao AVE! <harawata@gmail.com>
2017-12-18 01:46:19 +0900

    Apply method signature change.

--- src/main/java/test/CustomFileMigrationLoaderFactory.java
+++ src/main/java/test/CustomFileMigrationLoaderFactory.java
@@ -1,16 +1,14 @@
 package test;
 
-import java.io.File;
-import java.util.Properties;
-
+import org.apache.ibatis.migration.Environment;
 import org.apache.ibatis.migration.FileMigrationLoaderFactory;
 import org.apache.ibatis.migration.MigrationLoader;
+import org.apache.ibatis.migration.options.SelectedPaths;
 
 public class CustomFileMigrationLoaderFactory implements FileMigrationLoaderFactory {
-
   @Override
-  public MigrationLoader create(File scriptsDir, String charset, Properties variables) {
-    return new CustomFileMigrationLoader(scriptsDir, charset, variables);
+  public MigrationLoader create(SelectedPaths paths, Environment environment) {
+    return new CustomFileMigrationLoader(paths.getScriptPath(), environment.getScriptCharset(),
+        environment.getVariables());
   }
-
 }
