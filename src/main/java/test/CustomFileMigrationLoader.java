945fcccd5080550362c948db86d9d9f9e68d1b8e
Iwao AVE! <harawata@gmail.com>
2018-01-27 00:33:26 +0900

    Rewrote from scratch. Subclassing Change was a bad idea.

--- src/main/java/test/CustomFileMigrationLoader.java
+++ src/main/java/test/CustomFileMigrationLoader.java
@@ -1,16 +1,17 @@
 package test;
 
 import java.io.File;
-import java.math.BigDecimal;
+import java.io.IOException;
+import java.io.Reader;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Properties;
+import java.util.TreeMap;
 
 import org.apache.ibatis.migration.Change;
 import org.apache.ibatis.migration.FileMigrationLoader;
 import org.apache.ibatis.migration.MigrationException;
+import org.apache.ibatis.migration.MigrationReader;
 
 public class CustomFileMigrationLoader extends FileMigrationLoader {
 
@@ -20,93 +21,47 @@ public class CustomFileMigrationLoader extends FileMigrationLoader {
 
   @Override
   public List<Change> getMigrations() {
+    if (!scriptsDir.exists() || !scriptsDir.isDirectory()) {
+      throw new MigrationException("Scripts directory does not exist.");
+    }
+    TreeMap<String, File> scripts = new TreeMap<>();
+    collectScripts(scripts, scriptsDir);
     List<Change> migrations = new ArrayList<Change>();
-    if (scriptsDir.isDirectory()) {
-      List<String> relativePaths = listRelativePaths(scriptsDir, "", new ArrayList<String>());
-      if (relativePaths == null) {
-        throw new MigrationException(scriptsDir + " does not exist.");
-      }
-      Collections.sort(relativePaths, getRelativePathByFilenameComparator());
-      for (String relativePath : relativePaths) {
-        if (relativePath.endsWith(".sql") && !isSpecialFile(relativePath)) {
-          Change change = parseChangeFromFilename(relativePath);
-          migrations.add(change);
-        }
-      }
+    for (String name : scripts.keySet()) {
+      migrations.add(parseChangeFromFilename(name));
     }
     return migrations;
   }
 
-  private Comparator<String> getRelativePathByFilenameComparator() {
-    return new Comparator<String>() {
-      @Override
-      public int compare(String relativePath1, String relativePath2) {
-        return extractFilename(relativePath1).compareTo(extractFilename(relativePath2));
-      }
-    };
-  }
-
-  private int getLastIndexOfSlash(String relativePath) {
-    return relativePath.lastIndexOf("/") + 1;
-  }
-
-  private String extractFilename(String relativePath) {
-    return relativePath.substring(getLastIndexOfSlash(relativePath));
-  }
-
-  private String extractPath(String relativePath) {
-    return relativePath.substring(0, getLastIndexOfSlash(relativePath));
-  }
-
-  private List<String> listRelativePaths(File scriptsDir, String path, List<String> relativePathsAccumulator) {
-    File[] files = scriptsDir.listFiles();
-    for (File file : files) {
-      if (file.isFile()) {
-        relativePathsAccumulator.add(path + file.getName());
-      } else {
-        listRelativePaths(file, path + file.getName() + "/", relativePathsAccumulator);
-      }
+  @Override
+  public Reader getScriptReader(Change change, boolean undo) {
+    if (!scriptsDir.exists() || !scriptsDir.isDirectory()) {
+      throw new MigrationException(scriptsDir + " does not exist or is not a directory.");
     }
-    return relativePathsAccumulator;
-  }
-
-  protected Change parseChangeFromFilename(String filePath) {
+    TreeMap<String, File> scripts = new TreeMap<>();
+    collectScripts(scripts, scriptsDir);
+    File file = scripts.get(change.getFilename());
     try {
-      CustomChange change = new CustomChange();
-      String filename = extractFilename(filePath);
-      int lastIndexOfDot = filename.lastIndexOf(".");
-      String[] parts = filename.substring(0, lastIndexOfDot).split("_");
-      change.setId(new BigDecimal(parts[0]));
-      StringBuilder builder = new StringBuilder();
-      for (int i = 1; i < parts.length; i++) {
-        if (i > 1) {
-          builder.append(" ");
-        }
-        builder.append(parts[i]);
-      }
-      change.setDescription(builder.toString());
-      change.setFilename(filename);
-      change.setPath(extractPath(filePath));
-      return change;
-    } catch (Exception e) {
-      throw new MigrationException("Error parsing change from file.  Cause: " + e, e);
+      return new MigrationReader(file, charset, undo, variables);
+    } catch (IOException e) {
+      throw new MigrationException("Error reading " + file, e);
     }
   }
 
-  protected class CustomChange extends Change {
-    private String path;
-
-    @Override
-    public String getFilename() {
-      return getPath() + super.getFilename();
+  protected void collectScripts(TreeMap<String, File> scripts, File dir) {
+    File[] files = dir.listFiles();
+    if (files == null) {
+      throw new MigrationException("Error occured while listing files in directory: " + dir.getAbsolutePath());
     }
-
-    public String getPath() {
-      return path;
-    }
-
-    public void setPath(String path) {
-      this.path = path;
+    for (File f : files) {
+      if (f.isDirectory()) {
+        collectScripts(scripts, f);
+      } else if (f.isFile()) {
+        String name = f.getName();
+        if (name.endsWith(".sql") && !isSpecialFile(name)) {
+          scripts.put(f.getName(), f);
+        }
+      }
     }
   }
 }
